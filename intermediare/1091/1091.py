1ac4854cd90e748f04356baaa250503eb030e4e3
JoaoGabrielPereiraLopes <147850438+JoaoGabrielPereiraLopes@users.noreply.github.com>
2024-08-02 08:16:02 -0300

    Add files via upload

--- intermediare/1091/1091.py
+++ intermediare/1091/1091.py
@@ -1,22 +1,24 @@
+k=int(input())
 intermediaio=input().split(' ')
-print(intermediaio)
 n=int(intermediaio[0])
 m=int(intermediaio[1])
 resultados=[]
-resposta=''
-while not resposta=='0':
-    resposta=input()
-    if not resposta=='0':
-        resposta=[int(x) for x in resposta.split(' ')]
-        if resposta[0]<n and resposta[1]>m:
-            resultados+=['NO']
-        elif resposta[0]>n and resposta[1]>m:
-            resultados+=['NE']
-        elif resposta[0]<n and resposta[1]<m:
-            resultados+=['SO']
-        elif resposta[0]>n and resposta[1]<m:
-            resultados+=['SE']
-        else:
-            resultados+=['divisa']
-for x in resposta:
+k=k
+while not k==0:
+    for x in range(k):
+        resposta=input()
+        if not resposta=='0':
+            resposta=[int(x) for x in resposta.split(' ')]
+            if resposta[0]<n and resposta[1]>m:
+                resultados+=['NO']
+            elif resposta[0]>n and resposta[1]>m:
+                resultados+=['NE']
+            elif resposta[0]<n and resposta[1]<m:
+                resultados+=['SO']
+            elif resposta[0]>n and resposta[1]<m:
+                resultados+=['SE']
+            else:
+                resultados+=['divisa']
+    k=int(input())
+for x in resultados:
     print(x)
